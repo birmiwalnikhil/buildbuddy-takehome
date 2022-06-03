7bf56b12f96124ddd4ece386b7969a1d406bbfea
Nikhil Birmiwal <nikhilbirmiwal@gmail.com>
2022-06-03 23:13:12 +0000

    Add a client.Set method.

--- src/client/client.go
+++ src/client/client.go
@@ -1,7 +1,7 @@
 package client
 
 import (
-  "log"
+  "bytes"
   "io/ioutil"
   "net/http"
 )
@@ -18,14 +18,13 @@ type Client struct {
 
 /** 
  * A utility method for the HttpClient to invoke 
- * /get from the API server. Returns the stored
+ * /get on the API server. Returns the stored
  * value from a previous /set call, or an error 
  * on failure.
  */
-func (c *Client) Get (key string) ([]byte, error) {
+func (c *Client) Get(key string) ([]byte, error) {
   req, err := http.NewRequest("GET", "http://localhost:8080/get", nil)
   if err != nil {
-    log.Fatal(err)
     return emptyBuffer, err
   }
 
@@ -37,10 +36,28 @@ func (c *Client) Get (key string) ([]byte, error) {
   // Execute the request.
   resp, err := c.httpClient.Do(req) 
   if err != nil {
-    log.Fatal(err)
     return emptyBuffer, err
   }
 
+  // TODO: Case on the HttpErrorCode.
+
   defer resp.Body.Close()
   return ioutil.ReadAll(resp.Body)
+}
+
+/** 
+ * A utility method for the HttpClient to invoke /set 
+ * on the API server. Returns an error when invoking the request,
+ * if any.
+ */
+func (c *Client) Set(key string, value []byte) error {
+  _, err := 
+    http.Post(
+      "http://localhost:8080/set", "application/json", bytes.NewBuffer(value)) 
+  if err != nil {
+    return err
+  }
+
+  // TODO: Case on the HttpErrorCode
+  return nil
 }
