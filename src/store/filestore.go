40667bb657c7bc0cb5c3c1f59684b96bab8f5387
Nikhil Birmiwal <nikhilbirmiwal@gmail.com>
2022-06-05 20:01:06 +0000

    Add support for a temporary directory in the file store.
    Currently, server restart leaves the file store in a partial state; files that
    are semi-written will be considered valid after the server restarts. This means
    that the reader may receive incorrect data.
    
    This approach creates all files in a temporary directory, and on completion of
    writing the file, moves the file into the permanent directory. On FileStore
    instantation, all temporary files are deleted.

--- src/store/filestore.go
+++ src/store/filestore.go
@@ -9,16 +9,27 @@ import (
   "sync"
 )
 
+const (
+  TEMP_DIRECTORY_NAME = "tmp"
+)
+
 /**
  * A KeyValueStore that stores key/value pairs on disk.
  * Every key/value pair is allocated its own file.  
  *
  * <p> We enforce a deterministic strategy for identifying a 
  * filename given a Key. Currently, we filename for key == hash(key).
-*/
+ *
+ * <p> Files are created in a temporary subdirectory of `FileStore.directory`.
+ * On write completion, they are moved into `FileStore.directory`. This enables
+ * protection against partial writes due to server failure.  
+ */
 type FileStore struct {
-  // The absolute path where we should allocate files.
+  // The absolute path where which holds permanent files.
   directory string 
+  // The temporary directory which holds temporary files. Reset on
+  // instantiation of the file store. 
+  tempDirectory string
   // The hashing strategy for this file store.
   hash hash.Hash
   // A mutex used to synchronize access to the underlying file directory.
@@ -35,22 +46,30 @@ type FileStore struct {
 func (f *FileStore) Set(key Key, value Value) error {
     defer f.mutex.Unlock()
     f.mutex.Lock()
-    filePath := f.getFilePath(key)
-    file, err := 
-      os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
+
+    // 1) Create a temp file in the temporary directory.
+    // 2) Write to the temporary file.
+    // 3) On completion:
+    //    - Close the file.
+    //    - Promote the temporary file to a permanent file?
+    //    - Move the temporary file from the temp directory.
+    filePath := f.getFilePath(key, f.tempDirectory)
+    tmpFile, err := 
+      os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
     if err != nil {
       // IO Error when opening the file; return the error.
       return err
     }
-    
+   
     // Write the value into the opened file.
-    defer file.Close()
-    _, err2 := file.Write([]byte(value))
+    _, err2 := tmpFile.Write([]byte(value))
     if err2 != nil {
+      // On failure, close the opened file handle.
+      tmpFile.Close()
       return err2
     }
-
-    return nil
+  
+    return f.onTmpFileComplete(key, tmpFile)
 }
 
 /** 
@@ -60,7 +79,8 @@ func (f *FileStore) Set(key Key, value Value) error {
 func (f *FileStore) Get(key Key) (Value, error) {
   defer f.mutex.Unlock()
   f.mutex.Lock()
-  filePath := f.getFilePath(key)
+  // Only search the directory of fully written files.
+  filePath := f.getFilePath(key, f.directory)
   value, err := os.ReadFile(filePath)
   if err != nil {
     // Error when reading the file (e.g. corrupted file, file missing).
@@ -70,19 +90,49 @@ func (f *FileStore) Get(key Key) (Value, error) {
   return Value(value), nil
 }
 
-// Identify a file path given a Key. 
-func (f *FileStore) getFilePath(key Key) string {
+// Identify a file path given a Key. If isTmpFile is true, the path
+// will include the temporary directory. 
+func (f *FileStore) getFilePath(key Key, dir string) string {
   hash := f.hash.Sum([]byte(key))
-  return fmt.Sprintf(f.directory + "/%s", hex.EncodeToString(hash))
+  return fmt.Sprintf(dir + "/%s", hex.EncodeToString(hash))
+}
+
+// To be called on completion of writing to a temporary file, e.g.
+// to move the temporary file out of the temporary directory. Return
+// any errors, e.g. an OS failure.
+func (f *FileStore) onTmpFileComplete(key Key, tmpFile *os.File) error {
+  if err := tmpFile.Close(); err != nil {
+    return err
+  }
+
+  oldPath := f.getFilePath(key, f.tempDirectory)
+  newPath := f.getFilePath(key, f.directory)
+  if err := os.Rename(oldPath, newPath); err != nil {
+    return err
+  }
+  
+  return nil
 }
 
 func MakeFileStore(directory string) (*FileStore, error) {
   fs := &FileStore{}
   fs.directory = directory
+  fs.tempDirectory = fmt.Sprintf(directory + "/%s", TEMP_DIRECTORY_NAME) 
   // Make the directory if it does not already exist.
   if err := os.Mkdir(directory, 0644); err != nil && !os.IsExist(err) {
     return nil, err
   } 
+  
+  if err := os.RemoveAll(fs.tempDirectory); err != nil {
+    // Remove all the temporary files.
+    return nil, err
+  }
+
+  // Create the temp directory if it does not already exist.
+  if err := os.Mkdir(fs.tempDirectory, 0644); err != nil && !os.IsExist(err) {
+    return nil, err
+  }
+
   fs.mutex = &sync.Mutex{} 
   fs.hash = md5.New()
   return fs, nil
